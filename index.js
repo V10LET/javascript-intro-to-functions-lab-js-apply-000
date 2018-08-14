b1abbbba9f455572cc7fc607721da692c9269253
Violet Moon <violet.wmoon@gmail.com>
2018-08-14 23:19:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
     return "I can't hear you!"
+  } if else (string.toUpperCase()) {
+    return "YES, INDEED!"
+  } if else (sayHiToGrandma('I love you, Grandma.') {
+    return "I love you, too."
   }
 }
